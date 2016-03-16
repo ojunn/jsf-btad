a76ad928bd685c448dd89531d08774a42d62d2a0
junn <ojunn@github>
2016-03-17 02:18:55 +0900

    First commit

---
+++ judges_list.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         JSF btad Judges List
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @match        https://www.skatingjapan.jp/btad/admin_test_judge_reg.aspx
+// @require      https://code.jquery.com/jquery-1.12.0.min.js
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+//ボタンを押す度に描画しなおされるので毎回変更処理が必要
+function executeSearchOnValueChanged(){
+    var timeout_ms = 300
+    
+    /* 以下のCSSを適用すると快適
+    table {border-collapse: collapse;}
+    #ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(1) ,#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(2) ,#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(3) ,#ctl00_ContentPlaceHolder1_Panel3 table:nth-child(2) {display: none;}
+    #ctl00_ContentPlaceHolder1_Panel3 td.style34 {width: 120px;}
+    #ctl00_ContentPlaceHolder1_Panel3 td.style35 {width: 80px;}
+    select#ctl00_ContentPlaceHolder1_lstMember {height: 440px;}
+    #ctl00_ContentPlaceHolder1_UpdatePanel2 > table > tbody > tr:nth-child(2) > td.style25:first-child {width: 700px;}
+    #ctl00_ContentPlaceHolder1_UpdatePanel2 > table > tbody > tr:nth-child(2) > td.style25:first-child > div {height: 500px;overflow: scroll;}
+    */
+    //CSSを適用した場合は、hideとcssの分は不要。colspanとrowspanの変更は必要
+    $('#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(1)').hide()//登録番号と検索ボタン
+    $('#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(2), #ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(3)').hide()//名前（漢字）
+    //$('#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(4), #ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(5)').hide()//名前（ふりがな）
+    //$('#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(1) td:nth-child(3)').hide().prop('rowspan','3')
+    $('#ctl00_ContentPlaceHolder1_Panel3 table tr:nth-child(6) td').prop('colspan','2')
+    $('#ctl00_ContentPlaceHolder1_Panel3 table:nth-child(2)').hide()//追加ボタン
+    $('#ctl00_ContentPlaceHolder1_Panel3 td.style34').css("width", '120px')
+    $('#ctl00_ContentPlaceHolder1_Panel3 td.style35').css("width", '80px')
+    $('select#ctl00_ContentPlaceHolder1_lstMember').css("height", "440px")
+    $('#ctl00_ContentPlaceHolder1_UpdatePanel2 > table > tbody > tr:nth-child(2) > td:first-child').css({width: "700px"})
+    $('#ctl00_ContentPlaceHolder1_UpdatePanel2 > table > tbody > tr:nth-child(2) > td:first-child > div').css({overflow: "scroll", height: "500px"})
+
+    //検索の自動化、tabindex指定
+    $('#ctl00_ContentPlaceHolder1_Panel3 input[type=text]').change(function(e){
+        $('input#ctl00_ContentPlaceHolder1_btnSearch').click();
+        setTimeout(executeSearchOnValueChanged, timeout_ms)
+    }).each(function(i,el){
+        $(el).prop('tabindex', 200 + i)
+    })
+    $('input#ctl00_ContentPlaceHolder1_btnSearch').prop('tabindex', 100)
+    $('select#ctl00_ContentPlaceHolder1_lstMember').prop('tabindex', 500)
+
+    //検索結果の数で動作を変える
+    if( $('select#ctl00_ContentPlaceHolder1_lstMember option').length >= 1){//1名以上ヒット
+        var s = $('select#ctl00_ContentPlaceHolder1_lstMember')
+        if(s.prop('selectedIndex') <0) s.prop('selectedIndex', 0)
+        if($('select#ctl00_ContentPlaceHolder1_lstMember option').length ==1){
+            //1名ヒットした場合はその人を追加する
+            if($('#ctl00_ContentPlaceHolder1_lblMsg').text().length){
+                //ただし、すでに追加済みの場合は追加せずに入力欄にフォーカスを戻す
+                $('#ctl00_ContentPlaceHolder1_txtSeiKana').focus().select()
+            }else{
+                $('input#ctl00_ContentPlaceHolder1_btnAdd').click()
+                setTimeout(executeSearchOnValueChanged, timeout_ms)
+            }
+        }else{
+            //複数ヒットの場合は「名」にフォーカスする
+            //s.focus()
+            //$('#ctl00_ContentPlaceHolder1_txtMei').focus()
+            $('#ctl00_ContentPlaceHolder1_txtMeiKana').focus()
+        }            
+    }else{
+        //該当者なしの場合は入力欄にフォーカスを戻す
+        //$('#ctl00_ContentPlaceHolder1_txtSei').focus().select()
+        $('#ctl00_ContentPlaceHolder1_txtSeiKana').focus().select()
+    }
+    
+    //名前のクリックで追加する
+    $('select#ctl00_ContentPlaceHolder1_lstMember option').click(function(){
+        $('input#ctl00_ContentPlaceHolder1_btnAdd').click()
+        setTimeout(executeSearchOnValueChanged, timeout_ms)
+    })
+    
+    //名前のダブルクリックで追加する
+    $('select#ctl00_ContentPlaceHolder1_lstMember option').dblclick(function(){
+        $('input#ctl00_ContentPlaceHolder1_btnAdd').click()
+        setTimeout(executeSearchOnValueChanged, timeout_ms)
+    })
+    
+    //削除ボタンの押下で再描画されるので対応しておく
+    $('input[value=削除], input#ctl00_ContentPlaceHolder1_btnUpdate').click(function(e){
+        setTimeout(executeSearchOnValueChanged, timeout_ms)
+    })
+}
+
+executeSearchOnValueChanged()
+
---
+++ skaters_list.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         JSF btad Applicants
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @match        https://www.skatingjapan.jp/btad/admin_test_entry_list.aspx
+// @require      https://code.jquery.com/jquery-1.12.0.min.js
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+//幅指定解除
+$('table#ctl00_ContentPlaceHolder1_GridView').css('width','')
+
+var previousRank = ''
+$('table#ctl00_ContentPlaceHolder1_GridView tr').each(function(i,e){
+    var td = $(e).children('td,th')
+    var currentRank = $(td[3]).text()
+    
+    //級で分ける
+    if(previousRank != "" && currentRank != previousRank){
+        $('<tr><td colspan='+(td.length)+'></td></tr><tr style="height:4px;background-color:white !important;"><td></td><td></td><td colspan='+(td.length -2)+'></td>').insertBefore($(e))
+    }
+    if(currentRank == previousRank) $(td[3]).text('')
+    
+    //詳細ボタンを連番にする
+    $($(td[2]).children('input')).val(i).css({
+        width: '34px',
+        height: '30px',
+        fontWeight: 'bold'
+    }).parent().css('padding','0')
+//    $('<td style="text-align:right;width:1em;">'+( i>0 ? i : '' )+'</td>').prependTo(e)
+//    $(td[0]).remove()
+//    $(td[1]).remove()
+
+    //省スペース
+    $(td[3]).css('width','30px').text($(td[3]).text().replace(/受験/,""))
+    $(td[4]).css('width','80px')//名前
+    $(td[5]).css({width:'16px', backgroundColor: $(td[5]).text() == "○" ? 'orange' : 'inherit'}).text($(td[5]).text().replace(/○|備考/,"備"))
+    $(td[6]).text($(td[6]).text().replace(/^\s*$/,"仮").replace(/◎/,"-").replace(/登録/,"登")).css({width:'16px', backgroundColor: $(td[6]).text() == "仮" ? 'orange' : 'inherit'})
+    $(td[9]).text($(td[9]).text().replace(/20([0-9][0-9])/,"'$1"))
+    $(td[10]).text($(td[10]).text().replace(/20([0-9][0-9])/,"'$1"))
+    $(td[11]).text($(td[11]).text().replace(/20([0-9][0-9])/,"'$1"))
+    $(td[12]).css('width','240px')
+    previousRank = currentRank
+})
+
+/* CSS
+table {
+    border-collapse: collapse;
+}
+
+#aspnetForm > div:nth-child(9) > table > tbody > tr > td > table:nth-child(2) > tbody > tr > td > table > tbody > tr:nth-child(4) > td > contenttemplate > table > tbody > tr:nth-child(2) > td > div ,#ctl00_ContentPlaceHolder1_UpdatePanel2 > table:nth-child(2) > tbody > tr > td > div ,#aspnetForm > div:nth-child(18) > table > tbody > tr > td > table:nth-child(2) > tbody > tr > td > table > tbody > tr:nth-child(4) > td > contenttemplate > table:nth-child(1) > tbody > tr:nth-child(2) > td > div {
+    height: 1000px;
+    overflow-y: scroll;
+}
+#ctl00_ContentPlaceHolder1_UpdatePanel2 tr:nth-child(2n) {
+    background-color: #eee ;
+}
+#ctl00_ContentPlaceHolder1_UpdatePanel2 > div > table#ctl00_ContentPlaceHolder1_GridView tr th:nth-child(1) ,#ctl00_ContentPlaceHolder1_UpdatePanel2 > div > table#ctl00_ContentPlaceHolder1_GridView tr th:nth-child(2) ,#ctl00_ContentPlaceHolder1_UpdatePanel2 > div > table#ctl00_ContentPlaceHolder1_GridView tr td:nth-child(1) ,#ctl00_ContentPlaceHolder1_UpdatePanel2 > div > table#ctl00_ContentPlaceHolder1_GridView tr td:nth-child(2) {
+    display: none;
+}
+*/
